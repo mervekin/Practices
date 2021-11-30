364cef1b20a7f7e7218ede64496c140916eab35b
mervekin <bbozkurt.merve@gmail.com>
2021-11-30 16:05:15 +0300

    1

---
+++ Console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string  lastname=Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " "+ lastname);
+        }
+    }
+}
